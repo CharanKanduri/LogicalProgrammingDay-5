deb18394bb9867e73d7e8e0a45a3b003dd333251
charan_kanduri <charan2000kanduri@gmail.com>
2021-07-01 20:55:47 +0530

    [CHARAN]Leap year done

---
+++ BasicProgramming/FindLeapYear.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicProgramming
+{
+    class FindLeapYear
+    {
+        public static void ReadInput()
+        {
+            Console.WriteLine("Enter the Year: ");
+            int year = Convert.ToInt32(Console.ReadLine());
+
+            FindingLeapYear(year);
+
+
+
+
+        }
+        public static void FindingLeapYear(int year) 
+        {
+            if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0))
+            {
+                Console.WriteLine("{0} is a Leap Year.", year);
+            }
+            else Console.WriteLine("{0} is not a Leap Year.", year);
+        }
+
+    }
+}
---
+++ BasicProgramming/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace BasicProgramming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Basic Programming Examples");
+            Console.WriteLine("Please Select the option\n1.Find Leap Year\n2.Find Factors\n3.Find Vowel or Consonent\n4.Find Largest Among Three");
+
+            int chioce= Convert.ToInt32(Console.ReadLine());
+            switch (chioce)
+            {
+                case 1:
+                    FindLeapYear.ReadInput();
+                    break;
+                case 2:
+                    // code block
+                    break;
+
+                case 3:
+                    // code block
+                    break;
+
+                default:
+                    // code block
+                    break;
+            }
+
+        }
+    }
+}
